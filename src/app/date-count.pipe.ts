682c096c3172165c3d0b5b534b17f8526ec09813
Nelson Kimani <nelsonkimani96@gmail.com>
2019-09-21 12:09:31 +0300

    Return value in milliseconds

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -7,6 +7,8 @@ export class DateCountPipe implements PipeTransform {
 
   transform(value: any): number {
     let today: Date = new Date();
+    let todayWithNoTime: any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
+    var dateDifference = Math.abs(value - todayWithNoTime)
   
 
 }
