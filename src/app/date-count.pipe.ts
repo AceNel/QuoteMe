313ee90b23aa807647d39f7921d1bbf7f0ae1d6a
Nelson Kimani <nelsonkimani96@gmail.com>
2019-09-21 12:10:31 +0300

    return formatted date

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -9,6 +9,17 @@ export class DateCountPipe implements PipeTransform {
     let today: Date = new Date();
     let todayWithNoTime: any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
     var dateDifference = Math.abs(value - todayWithNoTime)
-  
+    const secondsInADay = 86400; //60 seconds*60 minutes in an hour *24 hours
 
+    var dateDifferenceSeconds = dateDifference * 0.001; //converts to seconds
+
+    var dateCounter = Math.round(dateDifferenceSeconds / secondsInADay);//avoid decimal number of days ago
+
+    if (dateCounter > 1) {
+      return dateCounter;
+    } else {
+      return 0;
+    }
+  }
 }
+
